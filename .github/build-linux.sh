253e55c6d28b0c612d9a6c06e4a88e7d32346e0b
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-02-05 10:47:19 -0500

    Fix artifact naming.
    We should be marking the Linux stuff as such.

--- .github/build-linux.sh
+++ .github/build-linux.sh
@@ -7,4 +7,4 @@ make -C /build/lib || exit 1
 
 cd /build || exit 1
 # shellcheck disable=SC2154
-tar -cvzf "libmosquitto-${tag}-${arch}".tar.gz lib/libmosquitto.a lib/*.h || exit 1
+tar -cvzf "libmosquitto-${tag}-linux-${arch}".tar.gz lib/libmosquitto.a lib/*.h || exit 1
